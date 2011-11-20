f0d5049a805263fcbcdc8cf08bb1db53fabf2222
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2011-11-20 13:19:29 +0100

    Added some code comments

--- Wheelmap/src/org/wheelmap/android/app/WheelmapApp.java
+++ Wheelmap/src/org/wheelmap/android/app/WheelmapApp.java
@@ -28,17 +28,32 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
+/**
+ * Application class for Wheelmap on Android
+ * This class initializes the ARCA framework (for crash reporting), the SupportManager
+ * (used for category and nodetype lookup), and the LocationManager.
+ * 
+ * It implements the Singleton pattern (using the static member "INSTANCE") and allows
+ * access to the central SupportManager.
+ * @author Michal Harakal, Michael Kroez
+ *
+ */
 // Beta and PRE-RC key: "dGJWQW5PelRXWUFTbDh6VW5UYm94cXc6MQ"
 // RC1 - key: @ReportsCrashes(formKey = "dC1VVDdKenJLRUpZTC1MZXBVR3p6ZlE6MQ" )
 // RC2 - key: @ReportsCrashes(formKey = "dG1fUDltTlNiM3V4NmRvaVExT3dJclE6MQ" )
 @ReportsCrashes( formKey = "dGMzcTRSZjRMRG14c0JmU25ET1JLQmc6MQ")
 public class WheelmapApp extends Application {
+	// used for debug logging
 	private final static String TAG = "wheelmapapp";
 	
 	private static WheelmapApp INSTANCE;
 	private MyLocationManager mLocationManager;
 	private SupportManager mSupportManager;
 	
+	/**
+	 * Called from the Android framework when the application is started through the launcher.
+	 * Initializes the SupportManager and the MyLocationManager singleton.
+	 */
 	@Override
 	public void onCreate() {
 		ACRA.init(this);
@@ -52,6 +67,10 @@ public class WheelmapApp extends Application {
 		INSTANCE = this;
 	}
 
+	/**
+	 * Called when the applications gets terminated. It detaches the MyLocationManager
+	 * from any event callbacks.
+	 */
 	@Override
 	public void onTerminate() {
 		super.onTerminate();
@@ -59,12 +78,21 @@ public class WheelmapApp extends Application {
 		Log.d(TAG,  "onTerminate" );
 	}
 	
+	/**
+	 * Called when the application is low on memory. Writes a debug message to ease debugging
+	 * issues related with low memory situations.
+	 */
 	@Override
 	public void onLowMemory() {
 		super.onLowMemory();
 		Log.d( "lowmemory", "wheelmap app - onLowMemory" );
 	}
 	
+	/**
+	 * Get the instance of the SupportManager. This has been initialized during
+	 * application startup and will _not_ be create on-demand!
+	 * @return single instance of SupportManager created during stratup
+	 */
 	public static SupportManager getSupportManager() {
 		if ( INSTANCE == null )
 			Log.d( TAG, "INSTANCE == null - how can that be?" );
